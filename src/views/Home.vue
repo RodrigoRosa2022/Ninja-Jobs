b6ac4e66a8f5d185d5df6bedfb34e7728d36cadd
RodrigoRosa2022 <ronibfr@gmail.com>
2023-09-28 20:03:36 -0300

    still building it

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint in ipsa eligendi odit et consequuntur veniam animi autem iste, omnis consequatur ipsum sapiente quam obcaecati? Quia, aliquam libero harum quibusdam officia, autem dolorum ex corrupti quas delectus quisquam fugit nemo eum, incidunt iure aspernatur veniam. Beatae corrupti quia doloribus ex nemo vero, magni magnam sit dicta voluptatibus, saepe et maxime, quos ipsam dolorem praesentium minus adipisci non deleniti eligendi aut iste. Mollitia sequi autem ducimus odio doloremque eveniet repellendus deleniti laudantium officia asperiores eaque, alias necessitatibus aliquam amet! Quia pariatur, dolor saepe rem quis labore nam consequatur odio obcaecati tenetur.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint in ipsa eligendi odit et consequuntur veniam animi autem iste, omnis consequatur ipsum sapiente quam obcaecati? Quia, aliquam libero harum quibusdam officia, autem dolorum ex corrupti quas delectus quisquam fugit nemo eum, incidunt iure aspernatur veniam. Beatae corrupti quia doloribus ex nemo vero, magni magnam sit dicta voluptatibus, saepe et maxime, quos ipsam dolorem praesentium minus adipisci non deleniti eligendi aut iste. Mollitia sequi autem ducimus odio doloremque eveniet repellendus deleniti laudantium officia asperiores eaque, alias necessitatibus aliquam amet! Quia pariatur, dolor saepe rem quis labore nam consequatur odio obcaecati tenetur.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {  }
 }
 </script>
