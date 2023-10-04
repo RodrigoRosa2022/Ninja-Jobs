f6c84ae1e178d0ccc625fef815b3f8e1c50db4d5
RodrigoRosa2022 <ronibfr@gmail.com>
2023-10-04 20:30:52 -0300

    simulando um fetch de json

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,10 +1,16 @@
 <template>
 <h1>Jobs</h1>
-<div v-for="job in jobs" :key="job.id" class="job">
+<div v-if="jobs.length">
+  <div v-for="job in jobs" :key="job.id" class="job">
   <router-link :to="{ name: 'JobDetails', params: { id: job.id} }">
     <h2>{{job.title}}</h2>
   </router-link>
+  </div>
 </div>
+<div v-else>
+  <p>Loading jobs...</p>
+</div>
+
 </template>
 
 <script>
@@ -17,6 +23,12 @@ export default {
         { title: 'Vue Developer', id:3 , details: 'lorem'},        
       ]
     }
+  },
+  mounted() {
+    fetch('http://localhost:3000/jobs')
+      .then(res => res.json())
+      .then(data => this.jobs = data)
+      .catch(err => console.log(err.message))
   }
 
 }
